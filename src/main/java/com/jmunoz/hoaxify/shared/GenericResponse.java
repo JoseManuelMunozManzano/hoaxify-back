128717582443b801487f84b681a67312f88c2dec
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-16 17:11:05 +0200

    User SignUp - ResponseBody red flag

---
+++ src/main/java/com/jmunoz/hoaxify/shared/GenericResponse.java
@@ -0,0 +1,8 @@
+package com.jmunoz.hoaxify.shared;
+
+import lombok.Data;
+
+@Data
+public class GenericResponse {
+    private String message;
+}
--- src/test/java/com/jmunoz/hoaxify/UserControllerTest.java
+++ src/test/java/com/jmunoz/hoaxify/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.jmunoz.hoaxify;
 
+import com.jmunoz.hoaxify.shared.GenericResponse;
 import com.jmunoz.hoaxify.user.User;
 import com.jmunoz.hoaxify.user.UserRepository;
 import org.junit.jupiter.api.*;
@@ -13,9 +14,6 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 // Test de integración
-// @ActiveProfiles sirve para darnos la flexibilidad de configurar el comportamiento de la app en tiempo de ejecución
-// Cuando ejecutamos los tests, queremos que se usen bases de datos de test, y no queremos usar nada de producción.
-// Queremos que nuestros test se ejecuten en un entorno controlado.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -29,9 +27,6 @@ public class UserControllerTest {
     @Autowired
     UserRepository userRepository;
 
-    // Cada ejecución debe correr en un entorno controlado, así que debe comenzar en un estado
-    // conocido y volver a ese estado tras ejecutar cada uno de los tests.
-    // Para eso se usan las anotaciones @BeforeEach y @AfterEach
     @BeforeEach
     void cleanup() {
         userRepository.deleteAll();
@@ -40,10 +35,6 @@ public class UserControllerTest {
     private User createValidUser() {
         User user = new User();
 
-        // Definamos que es un usuario válido
-        // username al menos de 4 caracteres
-        // displayName al menos 4 caracteres
-        // password con un carácter mayúsculas, uno minúsculas, un número y debe ser al menos de 8 caracteres
         user.setUsername("test-user");
         user.setDisplayName("test-display");
         user.setPassword("P4ssword");
@@ -57,23 +48,8 @@ public class UserControllerTest {
     public void postUser_whenUserIsValid_receiveOk() {
         User user = createValidUser();
 
-        // Decidimos que método http y que url vamos a usar.
-        // Como vamos a hacer una petición para crear un objeto, debemos usar el método POST.
-        //
-        // Para la url usamos users, en plural, porque es conveniente usar plurales en Rest.
-        // También es buena práctica añadir api y la versión del endpoint, ya que nuestra API evolucionará
-        // con el tiempo y queremos que los clientes que usen versiones anteriores puedan seguir usándola.
-        //
-        // El segundo parámetro es el objeto que vamos a crear (el POST)
-        //
-        // El tercero es la respuesta que esperamos recoger. Por ahora no lo sabemos, solo estamos
-        // interesados en saber si el resultado es 200 OK o no.
         ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
-
-        // Comprobamos si recibimos lo que queremos
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        // Tras crear lo mínimo necesario, la clase UserController con el endpoint, ya vemos que se pasa el test.
     }
 
     @Test
@@ -81,9 +57,19 @@ public class UserControllerTest {
         User user = createValidUser();
         testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
 
-        // Tenemos que confirmar si el usuario se ha almacenado en la BBDD
-        // Para ello, ejecutaremos nuestra query usando un objeto repository que crearemos
-        // cuando se ejecute el test y este falle (primero falla, después implementamos lo necesario)
         assertThat(userRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_receiveSuccessMessage() {
+        User user = createValidUser();
+
+        // Esperamos recibir como respuesta un GenericResponse, que todavía no existe, y lo creamos.
+        ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
+
+        // De la response obtenemos el body y en concreto el mensaje
+        // Como ahora mismo no obtenemos nada (null) este test falla.
+        assertThat(response.getBody().getMessage()).isNotNull();
+
     }
 }
