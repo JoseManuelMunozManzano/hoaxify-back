5aa48f3b1a18862c0216bff45ba31e81be17fa95
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-24 22:19:32 +0200

    Login - Authenticated User - Segunda forma usando inyectando Authentication en el método

--- src/main/java/com/jmunoz/hoaxify/user/LoginController.java
+++ src/main/java/com/jmunoz/hoaxify/user/LoginController.java
@@ -3,6 +3,7 @@ package com.jmunoz.hoaxify.user;
 import com.jmunoz.hoaxify.configuration.AuthUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,11 @@ public class LoginController {
     UserService userService;
 
     @PostMapping("/api/1.0/login")
-    Map<String, Object> handleLogin() {
+    Map<String, Object> handleLogin(Authentication authentication) {
         // Necesitamos obtener el usuario que hace log. Podemos hacerlo de 2 formas:
         // 1. Recuperándolo de SecurityContextHolder
-        CustomUserDetails customUserDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        // 2. Pidiendo a Spring que inyecte la autenticación a nuestro método
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
         User user = userService.findUserByUsername(customUserDetails.getUsername());
 
         // Devolvemos el id para que pase el test
