274c228c15586176230e23b18995d85a6815ac4f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-28 16:33:01 +0200

    Hoax Attachment - test a FileAttachmentRepository - si fecha de adjuntos mayor de una hora pero asociados a hoax no se devuelven

--- src/test/java/com/jmunoz/hoaxify/FileAttachmentRepositoryTest.java
+++ src/test/java/com/jmunoz/hoaxify/FileAttachmentRepositoryTest.java
@@ -2,6 +2,7 @@ package com.jmunoz.hoaxify;
 
 import com.jmunoz.hoaxify.file.FileAttachment;
 import com.jmunoz.hoaxify.file.FileAttachmentRepository;
+import com.jmunoz.hoaxify.hoax.Hoax;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -23,11 +24,20 @@ public class FileAttachmentRepositoryTest {
     @Autowired
     FileAttachmentRepository fileAttachmentRepository;
 
+    // Todos estos tests son para eliminar adjuntos no asociados a ningún hoax.
+    // Si el fichero lleva una hora creado y no tiene un hoax asociado, se eliminará.
+
     private FileAttachment getOneHourOldFileAttachment() {
         // Un milisegundo más de una hora
         Date date = new Date(System.currentTimeMillis() - (60*60*1000) - 1);
         FileAttachment fileAttachment = new FileAttachment();
         fileAttachment.setDate(date);
+        return fileAttachment;
+    }
+
+    private FileAttachment getOldFileAttachmentWithHoax(Hoax hoax) {
+        FileAttachment fileAttachment = getOneHourOldFileAttachment();
+        fileAttachment.setHoax(hoax);
         return fileAttachment;
     }
 
@@ -40,5 +50,20 @@ public class FileAttachmentRepositoryTest {
         Date oneHourAgo = new Date(System.currentTimeMillis() - (60*60*1000));
         List<FileAttachment> attachments = fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(oneHourAgo);
         assertThat(attachments.size()).isEqualTo(3);
+    }
+
+    @Test
+    void findByDateBeforeAndHoaxIsNull_whenAttachmentsDateOlderThanOneHourButHaveHoax_returnsNone() {
+        Hoax hoax1 = testEntityManager.persist(TestUtil.createValidHoax());
+        Hoax hoax2 = testEntityManager.persist(TestUtil.createValidHoax());
+        Hoax hoax3 = testEntityManager.persist(TestUtil.createValidHoax());
+
+        testEntityManager.persist(getOldFileAttachmentWithHoax(hoax1));
+        testEntityManager.persist(getOldFileAttachmentWithHoax(hoax2));
+        testEntityManager.persist(getOldFileAttachmentWithHoax(hoax3));
+
+        Date oneHourAgo = new Date(System.currentTimeMillis() - (60*60*1000));
+        List<FileAttachment> attachments = fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(oneHourAgo);
+        assertThat(attachments.size()).isEqualTo(0);
     }
 }
